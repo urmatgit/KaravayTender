9f82d400235f77dddb24aa23d275c4d7662dd5c3
urmatgit <urmat2g@gmail.com>
2021-09-28 10:51:02 +0300

    Add Request

---
+++ src/Domain/Entities/Karavay/ContragentCategory.cs
@@ -0,0 +1,24 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CleanArchitecture.Razor.Domain.Common;
+
+namespace CleanArchitecture.Razor.Domain.Entities
+
+{
+    public class ContragentCategory : AuditableEntity, IHasDomainEvent, IAuditTrial
+    {
+        
+        public int ContragentId { get; set; }
+        public virtual Contragent Contragent { get; set; }
+        public int CategoryId { get; set; }
+        public virtual Category Category { get; set; }
+        public List<DomainEvent> DomainEvents { get; set; }
+    }
+}
