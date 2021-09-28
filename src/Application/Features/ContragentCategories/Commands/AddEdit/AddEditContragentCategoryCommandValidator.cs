8f21957d6ff6f96f63081fb2dee52244a14619df
urmatgit <urmat2g@gmail.com>
2021-09-28 10:51:02 +0300

    Add Request

---
+++ src/Application/Features/ContragentCategories/Commands/AddEdit/AddEditContragentCategoryCommandValidator.cs
@@ -0,0 +1,16 @@
+using FluentValidation;
+
+namespace CleanArchitecture.Razor.Application.Features.ContragentCategories.Commands.AddEdit
+{
+    public class AddEditContragentCategoryCommandValidator : AbstractValidator<AddEditContragentCategoryCommand>
+    {
+        public AddEditContragentCategoryCommandValidator()
+        {
+           //TODO:Implementing AddEditContragentCategoryCommandValidator method 
+            //RuleFor(v => v.Name)
+            //     .MaximumLength(50)
+            //     .NotEmpty();
+           //throw new System.NotImplementedException();
+        }
+    }
+}
